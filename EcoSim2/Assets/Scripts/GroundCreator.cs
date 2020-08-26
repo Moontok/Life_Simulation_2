8fd79ce8f72161b0a08517ccd555c2ec3ffd043d
Moontok <zspink@nea.k12.ar.us>
2020-08-26 15:49:46 -0500

    First

---
+++ EcoSim2/Assets/Scripts/GroundCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCreator : EditorWindow
+{
+    [MenuItem("Custom/GenerateGround %g")]
+    public static void OpenWindow{
+        //GetWindow<GenerateGround>();
+    }
+
+
+}
+
+//https://forum.unity.com/threads/execute-a-script-once-in-editor.540772/
