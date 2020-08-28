21c6ceaec54afdee1d742126f0f436878e0e7ced
Moontok <zspink@nea.k12.ar.us>
2020-08-28 15:20:53 -0500

    Custom Inspector for Generator

--- EcoSim2/Assets/Scripts/CustomGroundCreatorInspector.cs
+++ EcoSim2/Assets/Scripts/CustomGroundCreatorInspector.cs
@@ -1,23 +1,45 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System;
 
 [CustomEditor(typeof(GroundCreator))]
 public class CustomGroundCreatorInspector : Editor
 {
+    GroundCreator creator;
+    SerializedProperty autoUpdate;
+    SerializedProperty tiles;
+
+    void OnEnable() 
+    {
+        creator = target as GroundCreator;
+        autoUpdate = serializedObject.FindProperty("autoUpdate");
+        tiles = serializedObject.FindProperty("tiles");
+        
+    }
+
     public override void OnInspectorGUI()
     {
-        DrawDefaultInspector();
-        GroundCreator creator = (GroundCreator)target;
-        // if(GUILayout.Button("Create Land"))
-        // {
-        //     creator.GenerateLand();
-        // }
+        serializedObject.Update();
 
-        // EditorGUILayout.LabelField("Delete");
-        
-        // if(GUILayout.Button("Delete Land"))
-        // {
-        //     creator.DeleteLand();
-        // }
+        DrawDefaultInspector();
+        for (int i = 0; i < tiles.arraySize; i++)
+        {
+            EditorGUILayout.Slider("Band "+i, 0, 0, 1);
+            
+        }
+
+        using (new EditorGUI.DisabledScope(autoUpdate.boolValue))
+        {
+            if(GUILayout.Button("Generate Land"))
+            {
+                creator.GenerateLand();
+            }
+            if(GUILayout.Button("Delete Land"))
+            {
+                creator.DeleteLand();
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
 }
--- EcoSim2/Assets/Scripts/GroundCreator.cs
+++ EcoSim2/Assets/Scripts/GroundCreator.cs
@@ -4,45 +4,57 @@ using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[System.Serializable]
 public class GroundCreator : MonoBehaviour
 {
 
-    [SerializeField] int landSize = 10;
+    [SerializeField] Vector2Int landSize = new Vector2Int(1,1);
     [SerializeField] GameObject[] tiles = new GameObject[0];
     [SerializeField] float tileSize = 1f;
+    [SerializeField] float xOrg = 0;
+    [SerializeField] float yOrg = 0;
+    [SerializeField] float noiseScale = 1f;    
+
+    [SerializeField] bool autoUpdate = true;
+    
+    [SerializeField] [Range(0,1)] float band1 = .3f;
+    [SerializeField] [Range(0,1)] float band2 = .6f;
     
     Vector3 startingTile = Vector3.zero;
     Vector3 currentTile = Vector3.zero;
-
+    
     void Update() 
-    {   
-        float x = (this.transform.position.x - landSize / 2) * tileSize;
-        float y = this.transform.position.y;
-        float z = (this.transform.position.z - landSize / 2) * tileSize;
+    {
+        CalcStartingTile();
 
-        startingTile = new Vector3(x, y, z);
-
-        if(Selection.Contains(this.gameObject) && this.isActiveAndEnabled) 
+        if(Selection.Contains(this.gameObject) && this.isActiveAndEnabled && autoUpdate) 
             GenerateLand();
 
     }
 
     public void GenerateLand()
     {
-        DeleteLand();     
+        DeleteLand();
 
-        for (int x = 0; x < landSize; x++)
+        float[,] tileNoise = CalcNoise();
+        GameObject tileToSet = null;
+
+        for (int x = 0; x < landSize.x; x++)
         {
             currentTile.z = startingTile.z;
-            for (int z = 0; z < landSize; z++)
+            for (int z = 0; z < landSize.y; z++)
             {
-                GameObject tile = Instantiate(tiles[0], currentTile, Quaternion.identity, this.gameObject.transform);
+                float tileNoiseSample = tileNoise[z,x];
+                if(tileNoiseSample < band1) tileToSet = tiles[0];
+                else if(tileNoiseSample < band2) tileToSet = tiles[1];
+                else tileToSet = tiles[2];
+
+                GameObject tile = Instantiate(tileToSet, currentTile, Quaternion.identity, this.gameObject.transform);
                 tile.transform.localScale = new Vector3(tileSize, 1, tileSize);
                 currentTile.z += tileSize;                
             }
             currentTile.x += tileSize;
-        }
-        
+        }        
     }
 
     public void DeleteLand()
@@ -60,9 +72,37 @@ public class GroundCreator : MonoBehaviour
         currentTile = startingTile;
     }
 
+    float[,] CalcNoise()
+    {        
+        float [,] tileNoise = new float[landSize.y, landSize.x];
+        float sample = 0f;
+
+        for (int y = 0; y < landSize.y; y++)
+        {
+            for (int x = 0; x < landSize.x; x++)
+            {
+                float xCoord = xOrg + (float)x / (float)landSize.x * noiseScale;
+                float yCoord = yOrg + (float)y / (float)landSize.y * noiseScale;
+                sample = Mathf.PerlinNoise(xCoord, yCoord);
+                tileNoise[y, x] = sample;                
+            }
+        }
+        return tileNoise;
+    }
+
+    void CalcStartingTile()
+    {
+        float coordx = this.transform.position.x - landSize.x * tileSize / 2 + tileSize / 2;
+        float coordy = this.transform.position.y;
+        float coordz = this.transform.position.z - landSize.y * tileSize / 2 + tileSize / 2;
+
+        startingTile = new Vector3(coordx, coordy, coordz);
+    }
+
     void OnValidate() 
     {
-        landSize = Mathf.Max(landSize, 0);
+        landSize.x = Mathf.Max(landSize.x, 1);
+        landSize.y = Mathf.Max(landSize.y, 1);
         tileSize = Mathf.Max(tileSize, 0);
     }
 }
